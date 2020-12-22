c32891a9eb9dbfea78f2feaec3a9e6446ae4f84b
Matěj Seykora <64471182+matejseykora@users.noreply.github.com>
2020-12-22 11:34:40 +0100

    Delete Main.java

--- Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println(AreaCalculator.area(4));
-        System.out.println(Math.PI);
-        System.out.println(Math.pow(4,2));
-    }
-}
