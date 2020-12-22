33350f29560b05e4a08c7d9ed9086aaceee80e2f
Matěj Seykora <64471182+matejseykora@users.noreply.github.com>
2020-12-22 11:30:41 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(AreaCalculator.area(4));
+        System.out.println(Math.PI);
+        System.out.println(Math.pow(4,2));
+    }
+}
